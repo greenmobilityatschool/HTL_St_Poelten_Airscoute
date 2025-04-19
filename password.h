558bca7f33b0c2f5e5547500a6c25ba0d08f1afb
Potzi <thomas.potzmader@gmail.com>
2025-04-20 00:57:31 +0200

    init

---
+++ password.h
@@ -0,0 +1,12 @@
+#ifndef PASSWORD_H
+#define PASSWORD_H
+
+#include <string>
+#include <vector>
+
+
+const std::vector<std::string> SSID = {"iPhone from Reiner", "Hotspotfrombemidafi"};
+const std::vector<std::string> PASS = {"12345678", "987654321"};
+
+
+#endif //PASSWORD_H
